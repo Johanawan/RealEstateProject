0466d4fd198fab0714f337b2a032c3e2ef91a399
Johan Awan <johanawan@hotmail.com>
2020-09-06 14:08:43 +0100

    Added search functionality

--- listings/views.py
+++ listings/views.py
@@ -35,10 +35,44 @@ def listing(request, listing_id):
     return render(request, 'listings/listing.html', context)
 
 def search(request):
+    queryset_list = Listing.objects.order_by('-list_date')
+
+    # Filter search function.
+    # Keywords
+    if 'keywords' in request.GET:
+        keywords = request.GET['keywords']
+        if keywords:
+            queryset_list = queryset_list.filter(description__icontains=keywords)
+
+    # City
+    if 'city' in request.GET:
+        city = request.GET['city']
+        if city:
+            queryset_list = queryset_list.filter(city__iexact=city)
+
+    # Area
+    if 'area' in request.GET:
+        area = request.GET['area']
+        if area:
+            queryset_list = queryset_list.filter(area__iexact=area)
+
+    # Bedrooms
+    if 'bedrooms' in request.GET:
+        bedrooms = request.GET['bedrooms']
+        if bedrooms:
+            queryset_list = queryset_list.filter(bedrooms__lte=bedrooms)
+    
+    # Price
+    if 'price' in request.GET:
+        price = request.GET['price']
+        if price:
+            queryset_list = queryset_list.filter(price__lte=price)
+
 
     context = {
         'area_choices': area_choices,
         'bedroom_choices': bedroom_choices,
         'price_choices': price_choices,
+        'listings': queryset_list,
     }
     return render(request, 'listings/search.html', context)
