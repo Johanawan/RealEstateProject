4913596e87ca1c67eaba1a82f655a7061a6db61a
Johan Awan <johanawan@hotmail.com>
2020-09-07 11:26:39 +0100

    Added registration & login page, added JS error message for registering

--- realestate/settings.py
+++ realestate/settings.py
@@ -136,3 +136,13 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+# LOGIN_REDIRECT_URL = 'index'
+# LOGOUT_REDIRECT_URL = 'index'
+
+# Messages
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'alert-danger',
+}
